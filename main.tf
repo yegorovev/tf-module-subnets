f04f6945d8b080e4098060171d09b17039a62cd1
yegorov <evgenyyegorov@gmail.com>
2023-07-10 20:32:40 +0400

    output was changed

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "subnets" {
   vpc_id                  = var.vpc_id
   cidr_block              = var.subnet_cidr
   availability_zone       = var.zone
